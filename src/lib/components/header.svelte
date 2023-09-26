eeb3699bdb7f66fb5bc9ab048cca7c2a9a3a9a4e
= <ryanneilparker@gmail.com>
2023-09-26 19:21:53 +0200

    Add header component

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,36 @@
+<script>
+	import ProfileImage from '$lib/images/profile-image.png';
+</script>
+
+<header class="container">
+	<article>
+		<header>
+			<img src={ProfileImage} alt="My beautiful face." />
+		</header>
+
+		<section>
+			<h1>Ryan Neil Parker</h1>
+			<h6>Solutions Architect | Software Engineer | Web Designer</h6>
+		</section>
+
+		<section class="social-bar">
+			<a href="https://github.com/ryanneilparker" role="button" class="outline">GitHub</a>
+			<a href="https://github.com/ryanneilparker" role="button" class="outline">Twitter</a>
+			<a href="https://github.com/ryanneilparker" role="button" class="outline">Youtube</a>
+		</section>
+
+		<footer>
+			<button><a class="call-to-action" href="/contact">Let's talk</a></button>
+		</footer>
+	</article>
+</header>
+
+<style>
+	img {
+		border-radius: 50%;
+	}
+
+	.call-to-action {
+		color: aliceblue;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<h1>Home</h1>
+<script>
+	import Header from '$lib/components/header.svelte';
+	import Main from '$lib/components/main.svelte';
+</script>
+
+<Header />
+
+<Main />
+
+<!-- <Footer /> -->
